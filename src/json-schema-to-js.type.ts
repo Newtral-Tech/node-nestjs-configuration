2c5806a1ad9985b002a947b5dcc7586b5d3e8d1d
Pablo Alvarez <pablo.alvarez@newtral.es>
2023-12-18 17:39:32 +0100

    feat(): update version

--- src/json-schema-to-js.type.ts
+++ src/json-schema-to-js.type.ts
@@ -5,15 +5,15 @@ export type JsonSchemaToJs<T extends JSONSchema7> = T extends {
 }
   ? string
   : T extends { type: 'number' } | { type: 'integer' }
-  ? number
-  : T extends { type: 'null' }
-  ? null
-  : T extends { type: 'boolean' }
-  ? boolean
-  : T extends { type: 'array'; items: JSONSchema7 }
-  ? Array<JsonSchemaToJs<T['items']>>
-  : T extends { type: 'object'; properties: Record<string, JSONSchema7> }
-  ? { [K in keyof T['properties']]: JsonSchemaToJs<T['properties'][K]> }
-  : T extends { oneOf: [JSONSchema7, JSONSchema7] }
-  ? { [K in keyof T['oneOf']]: JsonSchemaToJs<T['oneOf'][K]> }[number]
-  : unknown;
+    ? number
+    : T extends { type: 'null' }
+      ? null
+      : T extends { type: 'boolean' }
+        ? boolean
+        : T extends { type: 'array'; items: JSONSchema7 }
+          ? Array<JsonSchemaToJs<T['items']>>
+          : T extends { type: 'object'; properties: Record<string, JSONSchema7> }
+            ? { [K in keyof T['properties']]: JsonSchemaToJs<T['properties'][K]> }
+            : T extends { oneOf: [JSONSchema7, JSONSchema7] }
+              ? { [K in keyof T['oneOf']]: JsonSchemaToJs<T['oneOf'][any]> }[number]
+              : unknown;
